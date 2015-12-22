94635723816ee034d2f2dca39db441700cb4ef1b
iksharkov <i.k.sharkov@gmail.com>
2015-12-22 19:59:13 +0200

    task 2 teamwork

---
+++ task-2/templates/footer.php
@@ -0,0 +1,16 @@
+        <footer class="clearfix">
+        </footer>
+    </div><!-- End of wrapper -->
+    <script src="js/libs/jquery-1.8.3.min.js"></script>
+    <script>
+        if (typeof jQuery == 'undefined') {
+            var e = document.createElement('script');
+            e.src = "js/libs/jquery-1.8.3.min.js";
+            e.type='text/javascript';
+            document.getElementsByTagName("head")[0].appendChild(e);
+        }
+    </script>
+    <script src="js/plugins.js"></script>
+    <script src="js/application.js"></script>
+</body>
+</html>
