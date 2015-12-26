44f6facb19525901736baf6c3d8acc6fd5eb2f4b
Aleks359 <smu359@gmail.com>
2015-12-26 22:08:03 +0200

    some from footer

--- task-2/templates/footer.php
+++ task-2/templates/footer.php
@@ -1,9 +1,9 @@
         <footer class="clearfix">
             <div class="Social-Media">
                 <a href="https://twitter.com/" class="twitter" target="_blank"><img src="icons/twitter.png" alt="twitter"></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
+                <a href="" class="rrs" target="_blank"><img src="icons/rrs.png" alt="rrs"></a>
+                <a href="https://www.facebook.com/" class="facebook" target="_blank"><img src="icons/facebook.png" alt="pinterest"></a>
+                <a href="https://www.pinterest.com/" class="pinterest" target="_blank"><img src="icons/pinterest.png" alt="pinterest"></a>
             </div>
         </footer>
     </div><!-- End of wrapper -->
