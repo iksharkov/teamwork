661ba41a183ca83b0c78ec51771bf269b91d1306
iksharkov <i.k.sharkov@gmail.com>
2015-12-30 13:57:39 +0200

    home works

--- task-2/index.php
+++ task-2/index.php
@@ -3,6 +3,20 @@
 	include('templates/header.php');
 ?>
     <div class="slider">
-
+     <div class="first-row">
+        <img src="images/first_pic.jpg" alt="postcard">
+        <img src="images/second_pic.jpg" alt="drawings">
+        <img src="images/third_pic.jpg" alt="words">
+     </div>
+     <div class="second-row">
+         <img src="images/four_pic.jpg" alt="drawings">
+         <img src="images/five_pic.jpg" alt="hugs">
+         <img src="images/six_pic.jpg" alt="responsive">
+     </div>
+     <div class="third-row">
+         <img src="images/seven_pic.jpg" alt="lates news">
+         <img src="images/eight_pic.jpg" alt="words">
+         <img src="images/nine_pic.jpg" alt="drawings">
+     </div>
     </div>
 <?php include('templates/footer.php') ?>
