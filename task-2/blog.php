ed6c7a26766a51e1e77a2dcead7eabf940e9beb8
Aleks359 <smu359@gmail.com>
2016-01-02 22:15:15 +0200

    some news

--- task-2/blog.php
+++ task-2/blog.php
@@ -2,5 +2,72 @@
     $page_title = 'BLOG';
     include('templates/header.php');
 ?>
-    <h2>Read Our Latest Posts. Learn Something New Maybe!</h2>
+    <h2 class="clearfix">Read Our Latest Posts. Learn Something New Maybe!</h2>
+    <p class="list clearfix">All  /  News  /  Design  /  Print  /  Art  /  Development</p>
+    <div class="blog clearfix">
+        <div class="first-roll clearfix">
+            <div class="box">
+                <div class="imagee">
+                    <img src="images/Sticker_Mule.jpg" alt="Sticker_Mule">
+                </div>
+                <div class="text">
+                    <h3>Sticker Mule</h3>
+                    <div class="date">June 15, 2012 / news, contests</div>
+                    <p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores.</p>
+                </div>
+                <div class="submit">
+                    <img src="images/submit_arrow.jpg" alt="submitarrow">
+                    <button>More</button>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imagee">
+                    <img src="images/Amazing_websites.jpg" alt="Amazing_websites">
+                </div>
+                <div class="text">
+                    <h3>10 Amazing Websites</h3>
+                    <div class="date">June 15, 2012</div>
+                    <p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores.</p>
+                <div class="submit">
+                    <img src="images/submit_arrow.jpg" alt="submitarrow">
+                    <button>More</button>
+                </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imagee">
+                    <img src="images/Iphone_apps.jpg" alt="Iphone_apps">
+                </div>
+                <div class="text">
+                    <h3>Top iPhone Apps</h3>
+                    <div class="date">June 15, 2012</div>
+                    <p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores.</p>
+                    <div class="submit">
+                    <img src="images/submit_arrow.jpg" alt="submitarrow">
+                    <button>More</button>
+                </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imagee">
+                    <img src="images/Photo_Shoot.jpg" alt="Photo_Shoots">
+                </div>
+                <div class="text">
+                    <h3>Photo Shoot</h3>
+                    <div class="date">June 15, 2012</div>
+                    <p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores.</p>
+                    <div class="submit">
+                    <img src="images/submit_arrow.jpg" alt="submitarrow">
+                    <button>More</button>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="seccond-roll clearfix">
+            <div class="box"><img src="images/Big_Buck.jpg" alt="Big_Buck"></div>
+            <div class="box"><img src="images/Character_design.jpg" alt="Character_design"></div>
+            <div class="box"><img src="images/Service_icons.jpg" alt="Service_icons"></div>
+            <div class="box"><img src="images/Wedding_Card.jpg" alt="Wedding_Card"></div>
+        </div>
+    </div>
 <?php include('templates/footer.php') ?>
