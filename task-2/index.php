908d709254653c643be241e2893709be1299bbed
iksharkov <i.k.sharkov@gmail.com>
2016-01-05 15:58:22 +0200

    home hover effect

--- task-2/index.php
+++ task-2/index.php
@@ -3,20 +3,47 @@
 	include('templates/header.php');
 ?>
     <div class="slider">
-     <div class="first-row">
-        <img src="images/first_pic.jpg" alt="postcard">
-        <img src="images/second_pic.jpg" alt="drawings">
-        <img src="images/third_pic.jpg" alt="words">
-     </div>
-     <div class="second-row">
-         <img src="images/four_pic.jpg" alt="drawings">
-         <img src="images/five_pic.jpg" alt="hugs">
-         <img src="images/six_pic.jpg" alt="responsive">
-     </div>
-     <div class="third-row">
-         <img src="images/seven_pic.jpg" alt="lates news">
-         <img src="images/eight_pic.jpg" alt="words">
-         <img src="images/nine_pic.jpg" alt="drawings">
-     </div>
+     <ul class="first-row">
+        <li>
+            <img src="images/first_pic.jpg" alt="postcard">
+            <span class="text-content"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+        <li>
+            <img src="images/second_pic.jpg" alt="drawings" width="307" height="74">
+            <span class="text-content"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+        <li>
+            <img src="images/third_pic.jpg" alt="words">
+            <span class="text-content"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+     </ul>
+     <ul class="second-row">
+        <li>
+            <img src="images/four_pic.jpg" alt="drawings" width="626" height="299">
+            <span class="text-content-2"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+        <li>
+            <img src="images/five_pic.jpg" alt="hugs" width="307" height="183">
+            <span class="text-content-2"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+        <li>
+            <img src="images/six_pic.jpg" alt="responsive" width="307" height="110">
+            <span class="text-content-2"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+     </ul>
+     <ul class="third-row">
+        <li>
+            <img src="images/seven_pic.jpg" alt="lates news">
+            <span class="text-content-3"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+        <li>
+            <img src="images/eight_pic.jpg" alt="words" width="307" height="116">
+            <span class="text-content-3"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+        <li>
+            <img src="images/nine_pic.jpg" alt="drawings">
+            <span class="text-content-3"><span><img src="images/cross_hover.jpg"></span></span>
+        </li>
+     </ul>
     </div>
 <?php include('templates/footer.php') ?>
