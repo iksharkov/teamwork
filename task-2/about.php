259a8a22186013691fe12fa9e1c5c861416a9ad7
iksharkov <i.k.sharkov@gmail.com>
2016-01-14 01:28:00 +0200

    fixed

--- task-2/about.php
+++ task-2/about.php
@@ -9,31 +9,65 @@
 </div>
 <div class="pic-paragraph">
      <img src="images/about_pic_men..jpg" alt="about_pic_men">
-     <div>
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     <div class="para">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, deleniti quos,
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Ab dolore eum deleniti
+      quis ad quam architecto expedita vitae tempora, cumque provident quia eius
+       totam quibusdam sint aliquid doloremque. Iure, mollitia. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, deleniti quos,
      earum quaerat iusto nulla voluptatum labore vitae facilis, odit corporis
      impedit hic aliquid eligendi nihil dolores. Illo, doloribus! Sed. Lorem
-     ipsum dolor sit amet, consectetur adipisicing elit. Quidem distinctio cumque
+     ipsum dolor sit amet, consectetur adipisicing elit. Quidem distinctio Lorem
+     ipsum dolor sit amet, consectetur adipisicing elit. Enim minima ipsum deserunt debitis
+     quasi laborum doloremque ut doloribus sint, dolorem animi, necessitatibus tenetur
+     tempore similique mollitia, ea facilis recusandae autem. cumque
       corporis assumenda odio officia, ipsa soluta atque mollitia consectetur voluptati
       bus, adipisci ex, voluptate consequuntur id excepturi esse nam dolore.</p>
      </div>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat asperiores deleniti
+     sed nisi, ducimus, perspiciatis nam debitis eveniet officiis soluta dolor, doloremque modi
+     sed nisi, ducimus, perspiciatis nam debitis eveniet officiis soluta dolor, doloremque modi
+     assumenda placeat ex veritatis nulla explicabo mollitia Lorem ipsum dolor sit amet, consectetur
+     adipisicing elit. Ipsum veniam tempore labore temporibus, quam laboriosam inventore! Debitis
+     quos, ipsam recusandae accusamus expedita nam quidem cupiditate veritatis nesciunt..</p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum molestias animi a enim, quam,
+     pariatur aliquam aspernatur iste recusandae error tempore magnam. Ipsa ex facere praesentium
+     pariatur aliquam aspernatur iste recusandae error tempore magnam. Ipsa ex facere praesentium
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eligendi recusandae cumque
+     temporibus accusan iosam laudantium eius voluptates?</p>
+</div>
+<div class="big-d">
+    <p>D orem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quibusdam
+    voluptate perferendis, illum porro beatae consectetur iusto numquam nam ullam, Lorem ipsum
+    dolor sit amet, consectetur adipisicing elit. Nam, est provident, omnis vel natus, nisi accusamus
+    eaque repellendus labore nobis sequi accusantium ipsam ipsa obcaecati dolorem fugiat numquam
+    obcaecati, atque cupiditate eligendi aut assumenda maxime non reprehenderit.</p>
+</div>
+<div class="block-q"> <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloru
+    aut! Libero, praesentium qui tempora animi non earum Lorem ipsum dolor sit amet, consectetur adip
+    lit. Optio exercitationem saepe, accusamus, non officia deleniti magni sunt nihil expedita quisquam
+    pariatur doloribus voluptatibus quos doloremque nostrum, repudiandae assumenda libero nesciunt.
+    reprehenderit voluptates eaque rem! Minima ullam itaque ut, corporis provident earum error?</blockquote>
 </div>
 <div class="para-3">
  <div class="colum-one">
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Magnam quod, soluta consequatur fugit quae. Adipisci veritatis
-      rnatur natus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Magnam quod, soluta consequatur fugit quae. Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit. Explicabo, voluptates odio, inventore placeat et
+    beatae soluta facere corporis  quod, nam consectetur. Numquam, animi,
+    rnatur natus.</p>
  </div>
  <div class="colum-two">
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      provident. Accusamus sequi dolorem ea, explicabo optio,
-       in, autem voluptatem impedit libero magni laborum!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    provident. Accusamus sequi dolorem ea, explicabo optio,
+    in, autem voluptatem impedit libero magni laborum! Lorem ipsum dolor sit
+    reprehenderit aspernatur repellat aliquid assumenda alias eum, natus
+    quisquam corrupti deserunt quos voluptatibus. Ea vel, facere et ab. </p>
  </div>
 </div>
 <div class="para-4">
@@ -57,18 +91,18 @@
     </div>
 </div>
 <div class="buttons">
-    <button class="green">Buttom text</button>
-    <button class="silver">Buttom text</button>
-    <button class="blue">Buttom text</button>
-    <button class="orange">Buttom text</button>
-    <button class="purple">Buttom text</button>
-    <button class="red">Buttom text</button>
-    <button class="baby-blue">Buttom text</button>
+    <button class="button-1 green">Buttom text</button>
+    <button class="button-1 silver">Buttom text</button>
+    <button class="button-1 blue">Buttom text</button>
+    <button class="button-1 orange">Buttom text</button>
+    <button class="button-1 purple">Buttom text</button>
+    <button class="button-1 red">Buttom text</button>
+    <button class="button-1 baby-blue">Buttom text</button>
 </div>
-<div class="alert">
-    <div class="green-alert">Alert Text</div>
-    <div class="red-alert">Alert Text</div>
-    <div class="orange-alert">Alert Text</div>
-    <div class="blue-alert">Alert Text</div>
+<div class="alert-1">
+    <div class="alert-0 green-alert">Alert Text</div>
+    <div class="alert-0 red-alert">Alert Text</div>
+    <div class="alert-0 orange-alert">Alert Text</div>
+    <div class="alert-0 blue-alert">Alert Text</div>
 </div>
 <?php include('templates/footer.php') ?>
--- task-2/contacts.php
+++ task-2/contacts.php
@@ -5,7 +5,8 @@
 <div class="contact-header">
     <h2>Contact Us.Locate Us.Send Us Your Thoughts!</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odio adipisci temporibus expedita minus
-    voluptatum totam odit neces commodi id.</p>
+    voluptatum totam odit neces commodi idsum dolor sit amet, consectetur adipisicing elit. Velit odio
+    adipisci temporibus.</p>
 </div>
 <div id="map-canvas" style="height: 300px; width: 900px;"></div>
 <div class="imputs">
@@ -13,7 +14,7 @@
     <input type="text" name="First Name" placeholder="First Name" label="First Name">
   </div>
   <div>
-    <input type="text" name="Last Name" placeholder="last name" label="last name">
+    <input type="text" name="Last Name" placeholder="Last name" label="Last name">
   </div>
   <div>
     <input type="text" name="Email Address" placeholder="Email Address" label="Email Address">
@@ -21,6 +22,7 @@
     <textarea name="textarea" placeholder="Message" rows="10" cols="50"></textarea>
 </div>
 <div class="submit">
-  <a href="#"><img src="images/submit_arrow.jpg" alt="arrows">Submit</a>
+  <img src="images/submit_arrow.jpg" alt="arrows">
+  <input type="submit" value="Submit"/>
 </div>
 <?php include('templates/footer.php') ?>
